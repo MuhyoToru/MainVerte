bac0ebae2c41085b281d4d726970a1190edc6f4c
Pierre C <cilluffo.jeuxvideo@outlook.fr>
2024-08-01 10:48:23 +0200

    Initial Commit

---
+++ MainVerte_V002/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MainVerte_V002
+//
+//  Created by Apprenant 141 on 01/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MainVerte_V002/MainVerte_V002App.swift
@@ -0,0 +1,17 @@
+//
+//  MainVerte_V002App.swift
+//  MainVerte_V002
+//
+//  Created by Apprenant 141 on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MainVerte_V002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
